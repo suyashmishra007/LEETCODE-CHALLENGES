cee2922f3c925c6f87cdd10f41507a0c54a4bf49
suyashmishra007 <suyashmishra370@gmail.com>
2022-02-16 09:50:31 +0530

    16 feb

---
+++ February/16_Swap Nodes in Pairs.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+
+        if (!head or !head->next) {
+            return head;
+        }
+
+        ListNode *curr = head->next->next;
+        ListNode *prev = head;
+
+        head = head->next ;
+        head->next = prev ;
+
+        while (curr and curr->next) {
+            prev->next = curr->next ;
+            prev =  curr ;
+            ListNode * next = curr->next->next;
+            curr->next->next = curr;
+            curr = next ;
+        }
+
+        prev->next = curr ;
+        return head;
+
+    }
+};

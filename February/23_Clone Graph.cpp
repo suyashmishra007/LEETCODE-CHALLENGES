3af8c719809b3cc0be80c419aba498b4c4222790
suyashmishra007 <suyashmishra370@gmail.com>
2022-02-23 09:23:03 +0530

    23 Feb

---
+++ February/23_Clone Graph.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void dfs(Node* node , Node *copy ,vector<Node*> &vis){
+        vis[copy->val] = copy ;
+
+        for(auto nbr : node->neighbors){
+            if(vis[nbr->val] == NULL){
+                Node* newNode = new Node(nbr->val);
+                (copy->neighbors).push_back(newNode);
+                dfs(nbr,newNode,vis);
+            }else{
+                (copy->neighbors).push_back(vis[nbr->val] );
+            }
+        }
+    }
+    Node* cloneGraph(Node* node) {
+        if (node == NULL) {
+            return NULL ;
+        }
+
+        vector<Node*> vis(1000, NULL);
+        Node * copy = new Node(node-> val);
+        dfs(node, copy, vis);
+        return copy ;
+
+    }
+};
